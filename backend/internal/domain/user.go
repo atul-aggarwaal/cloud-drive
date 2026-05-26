aa2671094cdf1c525e9ad6bd6b44e74eeee91b28
atul-aggarwaal <aa.inbox-github@yahoo.com>
2026-05-26 23:05:48 +0530

    DRIVE-4 : Implemented User Repository to perform Create and Get user Details

---
+++ backend/internal/domain/user.go
@@ -0,0 +1,16 @@
+package domain
+
+import "context"
+
+// UserRepository defines the contract for User related operations
+type UserRepository interface {
+
+	//Creates a new User in the system
+	CreateUser(ctx context.Context, user *User) (string, error)
+	//Retrieves the user details using User's email ID
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+	//Retrieves User details using username, which is always unique
+	GetUserByName(ctx context.Context, name string) (*User, error)
+	//Retrieves User details using User's name
+	GetUserByID(ctx context.Context, name string) (*User, error)
+}
---
+++ backend/internal/repository/Postgres_user_repo.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/atul-aggarwaal/cloud-drive/internal/domain"
+)
+
+type PostresUserRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(db *sql.DB) *PostgresFileRepository {
+	return &PostgresFileRepository{db: db}
+}
+
+func (p PostresUserRepository) CreateUser(ctx context.Context, user *domain.User) (string, error) {
+	query := `INSERT INTO users(id, username, email, password_hash, created_at)
+				VALUES($1, $2, $3, $4, NOW();`
+	_, err := p.db.ExecContext(ctx, query, user.ID, user.UserName, user.Email, user.PasswordHash)
+
+	if err != nil {
+		return "", fmt.Errorf("user creation failed :%w", err)
+	}
+	return user.ID, nil
+}
+
+func (p PostresUserRepository) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	query := `SELECT id, username, email, password_hash, created_at FROM users WHERE email=$1`
+	row := p.db.QueryRowContext(ctx, query, email)
+
+	var user domain.User
+	err := row.Scan(user.ID, user.UserName, user.Email, user.PasswordHash, user.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+// TODO : Add uniqueness to userName, this can be login ID as well in future.
+func (p PostresUserRepository) GetUserByName(ctx context.Context, name string) (*domain.User, error) {
+	query := `SELECT id, username, email, password_hash, created_at FROM users WHERE username=$1`
+	row := p.db.QueryRowContext(ctx, query, name)
+
+	var user domain.User
+	err := row.Scan(user.ID, user.UserName, user.Email, user.PasswordHash, user.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (p PostresUserRepository) GetUserByID(ctx context.Context, userID string) (*domain.User, error) {
+	query := `SELECT id, username, email, password_hash, created_at FROM users WHERE id=$1`
+	row := p.db.QueryRowContext(ctx, query, userID)
+
+	var user domain.User
+	err := row.Scan(user.ID, user.UserName, user.Email, user.PasswordHash, user.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
