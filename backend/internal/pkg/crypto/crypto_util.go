bad89345e2d119fc634ed0d406936b0565de00f6
atul-aggarwaal <aa.inbox-github@yahoo.com>
2026-05-27 21:26:59 +0530

    DRIVE-7 : Implemented Crypto_util to support password encryption, verification and UUID7 generation

---
+++ backend/internal/pkg/crypto/crypto_util.go
@@ -0,0 +1,33 @@
+package crypto
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Generates a Time sorted UUID v7
+func GenerateUUID7() (string, error) {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return "", fmt.Errorf("failed to generate UUID: %w", err)
+	}
+	return id.String(), nil
+}
+
+// Encryptts plane text password into irreversible and cryptographically salted footprint
+func HashedPassword(plainText string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(plainText), 13)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate hashed password: %w", err)
+	}
+	return string(hashedBytes), nil
+}
+
+// Hash plain text passwordusing salt and cost embedded in hashed password and compare
+// output with hashed password, If output is same, password match is success else fail.
+func verifyPassword(password string, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err != nil
+}
