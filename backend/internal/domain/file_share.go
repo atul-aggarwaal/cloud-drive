f200bb65571ac59ce8530c0f22a17c5a96175bf7
atul-aggarwaal <aa.inbox-github@yahoo.com>
2026-06-09 22:54:48 +0530

    DRIVE-10: bug fixes

--- backend/internal/domain/file_share.go
+++ backend/internal/domain/file_share.go
@@ -1,12 +1,20 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 // Struct represents a single file share record
 type FileShare struct {
-	ID              string    `json:id`               //Unique Share ID
-	FileID          string    `json:file_id`          // ID of the file being shared
-	UserID          string    `json:user_id`          // ID of the user with whome file is being shared
-	permissionLevel string    `json:permission_level` // Level of permission to the user "read" / "read_write"
-	CreatedAt       time.Time `json:created_at`       // Time at which permission was given
+	ID               string    `json:"id"`               //Unique Share ID
+	FileID           string    `json:"file_id"`          // ID of the file being shared
+	SharedWithUserID string    `json:"user_id"`          // ID of the user with whome file is being shared
+	PermissionLevel  string    `json:"permission_level"` // Level of permission to the user "read" / "read_write"
+	CreatedAt        time.Time `json:"created_at"`       // Time at which permission was given
+}
+
+type FileShareRepository interface {
+	CreateShare(ctx context.Context, share *FileShare) error
+	HasAccess(ctx context.Context, fileId string, userId string) (bool, error)
 }
